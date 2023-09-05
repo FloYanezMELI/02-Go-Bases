770cb0f0f55c1a02cfb611ace0badba4323f5662
FloYanezMELI <florenciaandrea.yanez@mercadolibre.cl>
2023-09-05 17:23:51 -0300

    Se realizaron ejercicios de estructuras, métodos y composición

--- 04-go-bases/main/main.go
+++ 04-go-bases/main/main.go
@@ -1,9 +1,81 @@
+// https://playground.digitalhouse.com/course/1f38dad9-3a8d-4a1b-b6c3-818f36310664/unit/36c20045-2c4f-49af-93c5-d17185eff5aa/lesson/ca9970fd-8956-46ae-b42e-bbe306028e70/topic/53e0b7f8-0bce-4192-8c1e-412db6e69ae2
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+
+// Ejercicio 1
+var Products []Product = []Product{
+	{0, "Lapiz Azul", 1600.0, "Lapiz de Gel azúl marca Pentel", "Papelería"},
+	{1, "Corrector", 2000.0, "Corrector líquido marca PaperMate", "Papelería"},
+	{2, "Goma", 500.0, "Goma de borrar marca Faber Castell", "Papelería"},
+	{3, "Regla", 800.0, "Regla de medir plástica de 15cm marca Artel", "Papelería"},
+	{4, "Mouse", 10000.0, "Mouse inalámbrica a pilas marca Genius", "Tecnología"},
+}
+
+type Product struct {
+	ID          int
+	Name        string
+	Price       float64
+	Description string
+	Category    string
+}
+
+func (p Product) Save() {
+	Products = append(Products, p)
+}
+
+func (p Product) GetAll() {
+	for _, prod := range Products{
+		fmt.Println(prod)
+	}
+}
+
+func getById(id int) Product {
+	var product Product = Product{}
+	for _, prod := range Products {
+		if id == prod.ID {
+			fmt.Println(prod.ID)
+			product = prod
+		}
+	}
+	return product
+}
+
+// Ejercicio 2
+type Person struct {
+	ID int
+	Name string
+	DateOfBirth string
+}
+
+type Employee struct {
+	Person
+	ID int
+	Position string
+}
+
+func (e Employee) PrintEmployee() {
+	fmt.Println("ID de Persona:", e.Person.ID)
+	fmt.Println("Nombre:", e.Person.Name)
+	fmt.Println("Fecha de Nacimiento:", e.Person.DateOfBirth)
+	fmt.Println("ID de Empleado:", e.ID)
+	fmt.Println("Posición:", e.Position)
+}
 
 func main() {
-	//
+	// Ejercicio 1
+	fmt.Println("Ejercicio 1:")
+	producto1 := Product{5, "Colet", 1500.0, "Colet de tela con diseño floreado rojo marca Isadora", "Accesorios"}
+	producto1.Save()
+	producto1.GetAll()
+	fmt.Println(getById(2))
+
+	// Ejercicio 2
+	fmt.Println("\nEjercicio 2:")
+	person := Person{0, "Flo", "09/08/1996"}
+	employee1 := Employee{person, 1, "Software Developer"}
+	employee1.PrintEmployee()
+	employee2 := Employee{Person{1, "Eric", "23/02/2001"}, 2, "Software Developer"}
+	employee2.PrintEmployee()
 }
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ En este repositorio se encuentran separados por categoría distintos ejemplos y 
 * **01-go-bases**: variables, tipos de datos y operadores.
 * **02-go-bases**: estructuras de control.
 * **03-go-bases**: funciones y tests unitarios.
-* **04-go-bases**: 
+* **04-go-bases**: estructuras, métodos y composición.
